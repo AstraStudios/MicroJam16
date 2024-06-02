98373bcf07840b9611803c384433bd41c8d61709
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2024-06-01 19:34:00 -0500

    ghost mode

--- Assets/Scripts/GhostMode.cs
+++ Assets/Scripts/GhostMode.cs
@@ -6,14 +6,12 @@ using GLSLVectors;
 public class GhostMode : MonoBehaviour
 {
     bool isInGhostMode = false;
-    Collider2D playerCollider;
     string normalLayer = "Player";
     string ghostLayer = "Ghost";
 
     // Start is called before the first frame update
     void Start()
     {
-        playerCollider = GetComponent<BoxCollider2D>();
         gameObject.layer = LayerMask.NameToLayer(normalLayer);
     }
 
@@ -21,12 +19,10 @@ public class GhostMode : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown("q")) {isInGhostMode = !isInGhostMode; UpdatePlayerLayer();}
-
-        playerCollider.enabled = !isInGhostMode;
     }
 
     void UpdatePlayerLayer() {
         if (isInGhostMode) gameObject.layer = LayerMask.NameToLayer(ghostLayer);
-        if (isInGhostMode) gameObject.layer = LayerMask.NameToLayer(normalLayer);
+        else gameObject.layer = LayerMask.NameToLayer(normalLayer);
     }
 }
