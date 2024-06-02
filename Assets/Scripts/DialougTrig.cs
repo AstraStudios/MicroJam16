72d0c5db5757f4a37b6ec95763bc3f3f1203d6f7
sustachio <sustachio@gmail.com>
2024-06-02 12:38:51 -0500

    hospital level + fixe dude hitbox

--- Assets/Scripts/DialougTrig.cs
+++ Assets/Scripts/DialougTrig.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class DialougTrig : MonoBehaviour
 {
     [SerializeField] Dialoug dialoug;
+    [SerializeField] string activatorTag = "Player";
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag != "Player") return;
+        if (collision.tag != activatorTag) return;
 
         dialoug.enabled = true;
         Destroy(this);
